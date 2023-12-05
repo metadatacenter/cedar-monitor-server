84ddc3429bba6e02d2749eb9347d91ba9228319f
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2023-12-05 15:01:45 -0800

    Upgrade Mongo driver to 4.11.1. Change usage to align with new API

--- cedar-monitor-server-application/src/main/java/org/metadatacenter/cedar/monitor/MonitorServerApplication.java
+++ cedar-monitor-server-application/src/main/java/org/metadatacenter/cedar/monitor/MonitorServerApplication.java
@@ -1,6 +1,6 @@
 package org.metadatacenter.cedar.monitor;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClient;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
